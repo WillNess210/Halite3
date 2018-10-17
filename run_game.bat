70526391aac829eecce2cd7716f5870df9d19641
WillNess210 <WillNess210@gmail.com>
2018-10-16 19:42:05 -0600

    updated run script to use improved starter bot

--- run_game.bat
+++ run_game.bat
@@ -1,7 +1,7 @@
 cd C:\Users\WillN\Documents\College\CompetitiveProgramming\Halite3\Halite3_Java_Windows-x86
 call run_clean.bat
 javac MyBot.java
-javac StarterBot.java
-halite.exe --replay-directory replays/ -vvv --width 32 --height 32 "java MyBot" "java StarterBot"
+javac StarterBot2.java
+halite.exe --replay-directory replays/ -vvv --width 32 --height 32 "java MyBot" "java StarterBot2"
 rm *.class
 cp replays/*.hlt ./
