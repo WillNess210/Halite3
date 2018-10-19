30902e42ec69a106f085ae5f004f67f01896f912
WillNess210 <WillNess210@gmail.com>
2018-10-19 11:14:16 -0600

    commands are now static

--- StarterBot2.java
+++ StarterBot2.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 import java.util.Random;
-import hlt.*;
+import hlt2.*;
 
 public class StarterBot2{
 	public static void main(final String[] args){
@@ -12,29 +12,26 @@ public class StarterBot2{
 		}
 		final Random rng = new Random(rngSeed);
 		Game game = new Game();
-		// At this point "game" variable is populated with initial map data.
-		// This is a good place to do computationally expensive start-up pre-processing.
-		// As soon as you call "ready" function below, the 2 second per turn timer will
-		// start.
-		game.ready("MyJavaBot");
+		game.ready("Starter Bot");
 		Log.logln("Successfully created bot! My Player ID is " + game.myId + ". Bot rng seed is " + rngSeed + ".");
 		for(;;){
 			game.updateFrame();
+			CommandQueue.clear();
 			final Player me = game.me;
 			final GameMap gameMap = game.gameMap;
-			final ArrayList<Command> commandQueue = new ArrayList<>();
 			for(final Ship ship : me.ships.values()){
 				if(gameMap.at(ship).halite < Constants.MAX_HALITE / 10 || ship.isFull()){
 					final Direction randomDirection = Direction.ALL_CARDINALS.get(rng.nextInt(4));
-					commandQueue.add(ship.move(randomDirection));
+					CommandQueue.add(ship.move(randomDirection, gameMap));
 				}else{
-					commandQueue.add(ship.stayStill());
+					CommandQueue.add(ship.stayStill());
 				}
 			}
 			if(game.turnNumber <= 200 && me.halite >= Constants.SHIP_COST && !gameMap.at(me.shipyard).isOccupied()){
-				commandQueue.add(me.shipyard.spawn());
+				CommandQueue.add(me.shipyard.spawn());
 			}
-			game.endTurn(commandQueue);
+			game.endTurn();
+			Log.logln("");
 		}
 	}
 }
